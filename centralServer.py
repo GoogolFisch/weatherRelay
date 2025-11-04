599c29c9628b29d6be95e937d173cfd058e6e8e0
GoogolFisch <florian.losensky@yahoo.com>
2025-11-04 16:00:39 +0100

    adding scalable temperature capturing stuff

--- centralServer.py
+++ centralServer.py
@@ -1,60 +1,145 @@
 import time
 import requests
-from flask import Flask, render_template_string
-from threading import Timer
+from flask import Flask, render_template_string, render_template, Response
+from threading import Timer, Lock, Thread
+import select
+import socket
 
 app = Flask(__name__)
 
+
 # RaspyIP!!!
-PI_IPS = ['172.28.37.102', '172.18.99.160']  
+mutex = Lock()
+running = True
+
+reqAddresses = []
+reqSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+reqSocket.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
 PORT = 2680  
 
 sensor_data = {
-    'pi1': {'temperature': 0.0, 'humidity': 0.0, 'pressure': 0.0, 'timestamp': ''},
-    'pi2': {'temperature': 0.0, 'humidity': 0.0, 'pressure': 0.0, 'timestamp': ''}
 }
 
 # pre fetch html
 with open("WetterWeb.html") as fptr:
     html_template = fptr.read()
 
-def fetch_data_from_pis():
-    for i, ip in enumerate(PI_IPS, 1):
-        try:
-            url = f'http://{ip}:{PORT}/data'
-            response = requests.get(url, timeout=5)
-            if response.status_code == 200:
-                data = response.json()[0]
-                sensor_data[f'pi{i}'] = data
-                print(f"Daten von Pi{i} ({ip}) geholt: Temp={data['temperature']:.2f}°C")
-            else:
-                print(f"Fehler bei Pi{i} ({ip}): Status {response.status_code}")
-                sensor_data[f'pi{i}'] = {'temperature': -999, 'humidity': -999, 'pressure': -999, 'timestamp': 'Verbindung fehlgeschlagen'}
-        except Exception as e:
-            print(f"Verbindungsfehler zu Pi{i} ({ip}): {e}")
-            sensor_data[f'pi{i}'] = {'temperature': -999, 'humidity': -999, 'pressure': -999, 'timestamp': 'Verbindung fehlgeschlagen'}
-    
-    #reset nach 5sek (?)
-    Timer(5.0, fetch_data_from_pis).start() #!!!
+def get_pi_addresses(interval = 60):
+    #global reqAddresses
+    while True:
+        print("reload pis")
+        reqSocket.sendto(b"Hello",("172.31.255.255",2048))
+        time.sleep(1)
+        newAddresses = []
+        ignoreList = []
+        while True:
+            readable,writeable,extra = select.select([reqSocket],[],[],1)
+            if(len(readable) == 0):break
+            for s in readable:
+                name,otro = s.recvfrom(1024)
+                print(name,otro)
+                address,port = otro
+                if(address in ignoreList):
+                    continue
+                newAddresses.append((address,name))
+                ignoreList.append(address)
+        with mutex:
+            reqAddresses.clear()
+            reqAddresses.extend(newAddresses)
+            #reqAddresses = newAddresses
+            if(not running):break
+        print("updated pis")
+        #time.sleep(interval)
+        for _ in range(interval):
+            time.sleep(1)
+            if(not running):break
+    print("StoppGet")
+
+def fetch_data_from_pis(interval = 5):
+    cpAddresses = []
+    while True:
+        for ipAddr,piname in cpAddresses:
+            try:
+                url = f'http://{ipAddr}:{PORT}'
+                response = requests.get(url, timeout=5)#,max_retries=1)
+                if response.status_code == 200:
+                    data = response.json()[0]
+                    sensor_data[piname] = data
+                    print(f"Daten von Pi-{piname} ({ipAddr}) geholt: Temp={data['temperature']:.2f}°C")
+                else:
+                    print(f"Fehler bei Pi{i} ({ip}): Status {response.status_code}")
+                    #sensor_data[f'pi{i}'] = {'temperature': -999, 'humidity': -999, 'pressure': -999, 'timestamp': 'Verbindung fehlgeschlagen'}
+            except Exception as e:
+                print(f"Verbindungsfehler zu Pi-{piname} ({ipAddr}): {e}")
+                #sensor_data[f'pi{i}'] = {'temperature': -999, 'humidity': -999, 'pressure': -999, 'timestamp': 'Verbindung fehlgeschlagen'}
+        with mutex:
+            cpAddresses = reqAddresses
+            if(not running):break
+        #time.sleep(interval)
+        for _ in range(interval):
+            time.sleep(1)
+            if(not running):break
+    print("StoppFetch")
+
+@app.route('/file/<fileName>')
+def get_style_css(fileName):
+    if(".." in fileName):
+        return Respnse("No!")
+    with open(fileName,"r") as fptr:
+        data = fptr.read()
+    return Response(data,mimetype="text/css")
 
 @app.route('/')
 def index():
-    return render_template_string(html_template,
-        pi1_temperature=sensor_data['pi1']['temperature'],
-        pi1_humidity=sensor_data['pi1']['humidity'],
-        pi1_pressure=sensor_data['pi1']['pressure'],
-        pi1_timestamp=sensor_data['pi1']['timestamp'],
-        pi2_temperature=sensor_data['pi2']['temperature'],
-        pi2_humidity=sensor_data['pi2']['humidity'],
-        pi2_pressure=sensor_data['pi2']['pressure'],
-        pi2_timestamp=sensor_data['pi2']['timestamp']
+    #herePis = ""
+    #pi_temp = ""
+    #pi_hum = ""
+    #pi_pres = ""
+    #pi_time = ""
+    table_row = ""
+    for key,val in sensor_data.items():
+        #herePis += f"<th>{key}</th>"
+        #pi_temp += f"<td>{val['temperature']}</td>"
+        #pi_hum += f"<td>{val['humidity']}</td>"
+        #pi_pres += f"<td>{val['pressure']}</td>"
+        #pi_time += f"<td>{val['timestamp']}</td>"
+        table_row += f"<tr class=\"pi-data\">"
+        table_row += f"<td>{key}</td>"
+        table_row += f"<td>{val['temperature']}</td>"
+        table_row += f"<td>{val['humidity']}</td>"
+        table_row += f"<td>{val['pressure']}</td>"
+        table_row += f"<td>{val['timestamp']}</td>"
+
+        table_row += f"</tr>"
+
+    htmlData = ""
+    with open("WetterWeb.html","r")as fptr:
+        htmlData = fptr.read()
+    htmlData = htmlData.replace("$$$",table_row)
+
+    return render_template_string(htmlData,
+      #HerePis=herePis,
+      #pi_temperature=pi_temp,
+      #pi_humidity=pi_hum,
+      #pi_pressure=pi_pres,
+      #pi_timestamp=pi_time
     )
 
 if __name__ == '__main__':
     print("starte zentralen Server auf Port 5000")
     print("öffne im Browser: http://<IP HIER>:5000")
-    print("bitte IPs in PI_IPS anpassen")
-    # Starte ersten Datenabruf
-    fetch_data_from_pis()
+    # worker threads
+    timFetch = Thread(target=fetch_data_from_pis,args=(5,))
+    timGetPi = Thread(target=get_pi_addresses,args=(60,))
+    timFetch.start()
+    timGetPi.start()
     # Starte Server
     app.run(host='0.0.0.0', port=5000, debug=True)
+    print(timFetch.is_alive())
+    print(timGetPi.is_alive())
+    with mutex:
+        while(running):
+            running = False;time.sleep(0.1)
+    timFetch.join()
+    timGetPi.join()
+    print()
