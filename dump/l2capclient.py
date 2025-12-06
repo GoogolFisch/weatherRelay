e2e003b07a369e46db7887ad7bd6c9a7f17d8d1b
GoogolFisch <florian.losensky@yahoo.com>
2025-12-06 11:16:28 +0100

    adding potential future stuff

---
+++ dump/l2capclient.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""PyBluez simple example l2capclient.py
+
+Demo L2CAP client for bluetooth module.
+
+$Id: l2capclient.py 524 2007-08-15 04:04:52Z albert $
+"""
+
+import sys
+
+import bluetooth
+
+dev = bluetooth.discover_devices()
+print(dev)
+
+sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
+
+if len(sys.argv) < 2:
+    print(end="Use {dev[0]}? y/N>")
+    if(input().lower() == "y"):
+        sys.argv.append(dev[0])
+    else:
+        print("Usage: l2capclient.py <addr>")
+        sys.exit(2)
+
+bt_addr = sys.argv[1]
+port = 0x1001
+
+print("Trying to connect to {} on PSM 0x{}...".format(bt_addr, port))
+
+sock.connect((bt_addr, port))
+
+print("Connected. Type something...")
+while True:
+    data = input()
+    if not data:
+        break
+    sock.send(data)
+    data = sock.recv(1024)
+    print("Data received:", str(data))
+
+sock.close()
